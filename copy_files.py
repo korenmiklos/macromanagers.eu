b19e475d308c8f3307b33e532ad79b3b7cc3300b
Miklós Koren <miklos.koren@gmail.com>
2024-11-27 19:44:00 +0100

    Factor out python code from github action

---
+++ copy_files.py
@@ -0,0 +1,17 @@
+from filter_md import copy_markdown_files
+from filter_data import copy_data_files
+import os
+
+# Define collections to process
+collections = ['_posts', '_publications', '_courses', '_datasets', '_events', '_software']
+
+# Process markdown files in collections
+for collection in collections:
+    src = os.path.join('temp-remote', collection)
+    if os.path.exists(src):
+        copy_markdown_files(src, collection)
+
+# Process data files in _data
+src_data = os.path.join('temp-remote', '_data')
+if os.path.exists(src_data):
+    copy_data_files(src_data, '_data')
